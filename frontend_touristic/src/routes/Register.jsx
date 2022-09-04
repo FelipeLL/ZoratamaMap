1e7f23cbb197c65813f94af9c0a8e3d45310f279
FelipeLL <f3lip3.3z@hotmail.com>
2022-09-04 15:11:38 -0500

    se cambio nuevamente la url en Register frontend

--- frontend_touristic/src/routes/Register.jsx
+++ frontend_touristic/src/routes/Register.jsx
@@ -11,7 +11,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
-  const URI = "http://localhost:5000/api/users/register";
+  const URI = "https://mapzoratama.herokuapp.com/api/users/register";
 
   const {
     register,
