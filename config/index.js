1f6b0da796362adb7e60bea87155814e6b80d642
FelipeLL <f3lip3.3z@hotmail.com>
2022-09-03 20:50:17 -0500

    se agrego el host a una variable de entorno

--- config/index.js
+++ config/index.js
@@ -4,6 +4,7 @@ export const Config = {
   port: process.env.PORT,
   database: process.env.DATABASE,
   username: process.env.DB_USERNAME,
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   jwtSecret: process.env.JWT_SECRET,
   cookiesExpire: process.env.JWT_COOKIE_EXPIRES,
