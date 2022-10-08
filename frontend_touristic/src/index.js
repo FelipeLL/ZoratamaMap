13b771995105b47173d7a1cfc27ef8a8d90e2f09
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-07 21:16:48 -0500

    Se implemento la funcionalidad PWA

--- frontend_touristic/src/index.js
+++ frontend_touristic/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "mapbox-gl/dist/mapbox-gl.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./routes/Login";
 import UserProvider from './context/UserProvider';
@@ -35,7 +36,15 @@ ReactDOM.render(
   document.getElementById("root")
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    // Corremos este código si hay una nueva versión de nuestra app
+    // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
